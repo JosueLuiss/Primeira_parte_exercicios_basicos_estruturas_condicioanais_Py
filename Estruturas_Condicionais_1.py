71f4bc6c542bf8915e8e22e4901e4dd0b7109582
JosueLuiss <santossjosue21@gmail.com>
2025-07-10 15:42:33 -0300

    Create Estruturas_Condicionais_1.py

---
+++ Estruturas_Condicionais_1.py
@@ -0,0 +1,69 @@
+# ano_nascimento = int(input("Em que ano você nasceu ?"))
+# ano_atual = int(input("Em que ano nós estamos ? "))
+
+# idade = ano_atual - ano_nascimento
+
+# if idade < 18:
+#     print("Você tem {} anos. Você ainda é menor idade" .format(idade))
+# else:
+#     print("Você tem {} anos. Você já é maior idade " .format(idade))
+
+#-------------------------------------------------------------------------
+
+# num = int(input("Insira um número: "))
+
+# if num % 2 == 0:
+#     print("Esse número é par")
+# else:
+#     print("Esse número é ímpar ")
+
+#--------------------------------------------------------------------------
+
+# altura = float(input("Insira sua alura (m): "))
+# massa = float(input("Insira sua massa (Kg): "))
+
+# imc = massa / (altura * altura)
+
+# print("Seu imc é {}" .format(imc)) 
+
+# if imc < 17:
+#     print("Você está muito abaixo do peso")
+# if imc >= 17 and imc < 18.5:
+#     print("Você está abaixo do peso")
+# if imc >= 18.5 and imc < 25:
+#     print("Você está no peso ideal")
+# if imc >= 25 and imc < 30:
+#     print("Você está sobrepeso")
+# if imc >= 30 and imc < 35:
+#     print("Você está com obesidade")
+# if imc >= 35 and imc < 40:
+#     print("Você com obesidade severa")
+# if imc >= 40:
+#     print("Você está com obesidade Mórbida")
+
+#----------------------------------------------
+
+# ano_nascimento = int(input("Em que ano você nasceu? "))
+# ano_atual = int(input("Em que ano estamos? "))
+
+# idade = ano_atual - ano_nascimento
+
+# if idade >= 18:
+#     print("Você tem {} anos. Está apto a dirigir" .format(idade))
+# else:
+#     print("Você tem {} anos. Não está apto a dirigir" .format(idade))
+
+#-----------------------------------------------------------------------
+
+# n1 = float(input("Insira a primeira nota: "))
+# n2 = float(input("Insira a segunda nota:"))
+# n3 = float(input("Insira a terceira nota: "))
+# n4 = float(input("insira a quarta nota: "))
+
+# media = (n1 + n2 + n3 + n4) / 4
+# print("Média: {}" .format(media))
+
+# if media < 7:
+#     print("Reprovado")
+# else:
+#     print("Aprovado")
